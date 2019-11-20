856bc83f96c4e59734cc799befd9b0bb1b247d08
Bikram <bikram.aditya2099@gmail.com>
2019-11-20 10:15:48 +0530

    removed main method

--- TheCodersNation/src/main/java/com/codersnation/util/EmailUtil.java
+++ TheCodersNation/src/main/java/com/codersnation/util/EmailUtil.java
@@ -137,36 +137,36 @@ public class EmailUtil {
 	        return PINString;
 	    }
 
-	public static void main(String[] args) {
-		final String fromEmail = "admin@thecodersnation.com"; //requires valid gmail id
-		final String password = "admin@123"; // correct password for gmail id
-		final String toEmail = "bikram.aditya2099@gmail.com"; // can be any email id 
-		
-		System.out.println("SSLEmail Start");
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "mail.thecodersnation.com"); //SMTP Host
-		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
-		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
-		props.put("mail.smtp.port", "465"); //SMTP Port
-		
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(fromEmail, password);
-			}
-		};
-		
-		Session session = Session.getDefaultInstance(props, auth);
-		System.out.println("Session created");
-	      //  EmailUtil.sendEmail(session,fromEmail, toEmail,"SSLEmail Testing Subject", createRegistrationTemplate("Bikram",generatePIN()));
-
-	    //    EmailUtil.sendAttachmentEmail(session,fromEmail, toEmail,"SSLEmail Testing Subject with Attachment", "SSLEmail Testing Body with Attachment");
-
-	    //    EmailUtil.sendImageEmail(session,fromEmail, toEmail,"SSLEmail Testing Subject with Image", "SSLEmail Testing Body with Image");
-
-	}
+//	public static void main(String[] args) {
+//		final String fromEmail = "admin@thecodersnation.com"; //requires valid gmail id
+//		final String password = "admin@123"; // correct password for gmail id
+//		final String toEmail = "bikram.aditya2099@gmail.com"; // can be any email id 
+//		
+//		System.out.println("SSLEmail Start");
+//		Properties props = new Properties();
+//		props.put("mail.smtp.host", "mail.thecodersnation.com"); //SMTP Host
+//		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+//		props.put("mail.smtp.socketFactory.class",
+//				"javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+//		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+//		props.put("mail.smtp.port", "465"); //SMTP Port
+//		
+//		Authenticator auth = new Authenticator() {
+//			//override the getPasswordAuthentication method
+//			protected PasswordAuthentication getPasswordAuthentication() {
+//				return new PasswordAuthentication(fromEmail, password);
+//			}
+//		};
+//		
+//		Session session = Session.getDefaultInstance(props, auth);
+//		System.out.println("Session created");
+//	      //  EmailUtil.sendEmail(session,fromEmail, toEmail,"SSLEmail Testing Subject", createRegistrationTemplate("Bikram",generatePIN()));
+//
+//	    //    EmailUtil.sendAttachmentEmail(session,fromEmail, toEmail,"SSLEmail Testing Subject with Attachment", "SSLEmail Testing Body with Attachment");
+//
+//	    //    EmailUtil.sendImageEmail(session,fromEmail, toEmail,"SSLEmail Testing Subject with Image", "SSLEmail Testing Body with Image");
+//
+//	}
 	
 	public static void sendEmail( String toEmail, String subject, String body){
 		try
